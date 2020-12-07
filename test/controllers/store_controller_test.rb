89f876da08af00abde55839ebb8f191d0ae44a22
Hong Xiao <jeffhongxiao@gmail.com>
2020-12-07 19:54:12 -0400

    Iteration C4: functional testing of Controllers

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select 'nav.side_nav a', minimum: 3
+    assert_select 'main ul.catalog li', minimum: 3
+    assert_select 'h2', 'Book'
+    assert_select '.price', /\$[,\d]+\.\d\d/
   end
 
 end
