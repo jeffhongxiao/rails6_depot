071a8b53f01b461a997e9b7a8d99acf548109d75
Hong Xiao <jeffhongxiao@gmail.com>
2020-12-08 19:07:45 -0400

    D1: db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_231450) do
+ActiveRecord::Schema.define(version: 2020_12_08_230327) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
