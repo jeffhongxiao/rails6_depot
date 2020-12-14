169f9dfd13befb28868446957880364afd5472e0
Hong Xiao <jeffhongxiao@gmail.com>
2020-12-14 19:44:00 -0400

    Iteration G1: fix enum to form.select

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,7 +8,7 @@ class Order < ApplicationRecord
   has_many :line_items, dependent: :destroy
 
   validates :name, :address, :email, presence: true
-  validates :pay_type, inclusion: pay_types.keys
+  validates :pay_type, inclusion: pay_types.values
 
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
